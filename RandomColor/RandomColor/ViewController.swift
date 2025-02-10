2a5532c4de554a2cef8ed3a3f5c277d359e67f5b
youseokhwan <youseokhwan15@gmail.com>
2025-02-10 17:45:21 +0900

    feat: changeColor 버튼, reset 버튼 추가

--- RandomColor/RandomColor/ViewController.swift
+++ RandomColor/RandomColor/ViewController.swift
@@ -8,13 +8,45 @@ final class ViewController: UIViewController {
         return label
     }()
 
+    private lazy var changeColorButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Change Color", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    private lazy var resetButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Reset", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    private lazy var buttonsStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.addSubview(colorLabel)
+        buttonsStackView.addArrangedSubview(changeColorButton)
+        buttonsStackView.addArrangedSubview(resetButton)
+        view.addSubview(buttonsStackView)
+
         NSLayoutConstraint.activate([
             colorLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             colorLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        NSLayoutConstraint.activate([
+            buttonsStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            buttonsStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            buttonsStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            buttonsStackView.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
 }
