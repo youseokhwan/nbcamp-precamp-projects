6ce43ba89e2d3e631f6682fc376b9cb1e063a15f
youseokhwan <youseokhwan15@gmail.com>
2025-02-10 17:35:38 +0900

    feat: colorLabel 추가

--- RandomColor/RandomColor/ViewController.swift
+++ RandomColor/RandomColor/ViewController.swift
@@ -1,7 +1,20 @@
 import UIKit
 
 final class ViewController: UIViewController {
+    private lazy var colorLabel: UILabel = {
+        let label = UILabel()
+        label.text = "현재 RGB값: x, y, z"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        view.addSubview(colorLabel)
+        NSLayoutConstraint.activate([
+            colorLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            colorLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
 }
