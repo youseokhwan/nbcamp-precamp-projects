fc48f6abf99e1bd6eda7a2dd2707ca49b3ec55e2
youseokhwan <youseokhwan15@gmail.com>
2025-02-13 14:27:52 +0900

    refactor: UI 컴포넌트 분리

--- RandomColor/RandomColor/Controllers/ViewController.swift
+++ RandomColor/RandomColor/Controllers/ViewController.swift
@@ -3,32 +3,10 @@ import UIKit
 final class ViewController: UIViewController {
     private var color = Color()
 
-    private lazy var colorLabel: UILabel = {
-        let label = UILabel()
-        label.text = color.rgbText
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    private lazy var changeColorButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Change Color", for: .normal)
-        button.addTarget(self, action: #selector(tappedChangeColorButton), for: .touchUpInside)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-    private lazy var resetButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Reset", for: .normal)
-        button.addTarget(self, action: #selector(tappedResetButton), for: .touchUpInside)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-    private lazy var buttonsStackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .horizontal
-        stackView.distribution = .fillEqually
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        return stackView
+    private lazy var colorView: ColorView = {
+        let colorView = ColorView()
+        colorView.translatesAutoresizingMaskIntoConstraints = false
+        return colorView
     }()
 
     override func viewDidLoad() {
@@ -47,7 +25,7 @@ final class ViewController: UIViewController {
     @objc private func didUpdatedRGB() {
         DispatchQueue.main.async {
             self.view.backgroundColor = UIColor(self.color)
-            self.colorLabel.text = self.color.rgbText
+//            self.colorLabel.text = self.color.rgbText
         }
     }
 }
@@ -60,22 +38,15 @@ private extension ViewController {
     }
 
     func configureViews() {
-        view.addSubview(colorLabel)
-        view.addSubview(buttonsStackView)
-        buttonsStackView.addArrangedSubview(changeColorButton)
-        buttonsStackView.addArrangedSubview(resetButton)
+        view.addSubview(colorView)
     }
 
     func configureConstraints() {
         NSLayoutConstraint.activate([
-            colorLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            colorLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
-        NSLayoutConstraint.activate([
-            buttonsStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            buttonsStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            buttonsStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
-            buttonsStackView.heightAnchor.constraint(equalToConstant: 50)
+            colorView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            colorView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            colorView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            colorView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
     }
 
--- RandomColor/RandomColor/Views/ColorView.swift
+++ RandomColor/RandomColor/Views/ColorView.swift
@@ -1,5 +1,68 @@
 import UIKit
 
 final class ColorView: UIView {
+    private lazy var colorLabel: UILabel = {
+        let label = UILabel()
+        label.text = ""
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    private lazy var changeColorButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Change Color", for: .normal)
+//        button.addTarget(self, action: #selector(tappedChangeColorButton), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    private lazy var resetButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Reset", for: .normal)
+//        button.addTarget(self, action: #selector(tappedResetButton), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    private lazy var buttonsStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configures()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configures()
+    }
+}
+
+private extension ColorView {
+    func configures() {
+        configureViews()
+        configureConstraints()
+    }
+
+    func configureViews() {
+        addSubview(colorLabel)
+        addSubview(buttonsStackView)
+        buttonsStackView.addArrangedSubview(changeColorButton)
+        buttonsStackView.addArrangedSubview(resetButton)
+    }
+
+    func configureConstraints() {
+        NSLayoutConstraint.activate([
+            colorLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            colorLabel.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+        NSLayoutConstraint.activate([
+            buttonsStackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            buttonsStackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            buttonsStackView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            buttonsStackView.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
 }
