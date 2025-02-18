77bf6e8bef63dbfa71f720eaa35b2d93207a148d
youseokhwan <youseokhwan15@gmail.com>
2025-02-18 17:44:59 +0900

    feat: UI 구현

--- Counter/Counter/ViewController.swift
+++ Counter/Counter/ViewController.swift
@@ -1,7 +1,66 @@
 import UIKit
+import SnapKit
 
 final class ViewController: UIViewController {
+    private lazy var countLabel: UILabel = {
+        let label = UILabel()
+        label.text = "\(0)"
+        label.font = .systemFont(ofSize: 32)
+        return label
+    }()
+    private lazy var buttonsStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        return stackView
+    }()
+    private lazy var plusButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("+", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 24)
+        return button
+    }()
+    private lazy var minusButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("-", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 24)
+        return button
+    }()
+    private lazy var resetButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Reset", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 24)
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        configure()
+    }
+}
+
+private extension ViewController {
+    func configure() {
+        configureViews()
+        configureConstraints()
+    }
+
+    func configureViews() {
+        view.addSubview(countLabel)
+        view.addSubview(buttonsStackView)
+        buttonsStackView.addArrangedSubview(plusButton)
+        buttonsStackView.addArrangedSubview(minusButton)
+        buttonsStackView.addArrangedSubview(resetButton)
+    }
+
+    func configureConstraints() {
+        countLabel.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        buttonsStackView.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview().inset(100)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(50)
+            make.height.equalTo(50)
+        }
     }
 }
