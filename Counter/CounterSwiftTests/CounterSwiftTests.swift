26e93372ee53ee837ae28a22b957778bf5302d8b
youseokhwan <youseokhwan15@gmail.com>
2025-02-18 23:59:31 +0900

    feat: CounterSwiftTests TC 작성

--- Counter/CounterSwiftTests/CounterSwiftTests.swift
+++ Counter/CounterSwiftTests/CounterSwiftTests.swift
@@ -1,7 +1,61 @@
 import Testing
 
 struct CounterSwiftTests {
-    @Test func example() async throws {
+    var viewModel: MainViewModel!
 
+    init() async throws {
+        viewModel = MainViewModel()
+    }
+
+    @Test func 초기_상태() async throws {
+        #expect(viewModel.count == 0)
+        #expect(viewModel.plusButtonIsEnabled)
+        #expect(viewModel.minusButtonIsEnabled)
+    }
+
+    @Test func 카운트_증가() async throws {
+        viewModel.increaseCount()
+        #expect(viewModel.count == 1)
+    }
+
+    @Test func 카운트_감소() async throws {
+        viewModel.decreaseCount()
+        #expect(viewModel.count == -1)
+    }
+
+    @Test func 카운트_리셋() async throws {
+        viewModel.increaseCount()
+        viewModel.increaseCount()
+        viewModel.increaseCount()
+        viewModel.resetCount()
+        #expect(viewModel.count == 0)
+    }
+
+    @Test func 카운트_최댓값() async throws {
+        (0...9).forEach { _ in
+            viewModel.increaseCount()
+        }
+        #expect(viewModel.count == 10)
+        #expect(!viewModel.plusButtonIsEnabled)
+        viewModel.increaseCount()
+        #expect(viewModel.count == 10)
+        #expect(!viewModel.plusButtonIsEnabled)
+        viewModel.decreaseCount()
+        #expect(viewModel.count == 9)
+        #expect(viewModel.plusButtonIsEnabled)
+    }
+
+    @Test func 카운트_최솟값() async throws {
+        (0...9).forEach { _ in
+            viewModel.decreaseCount()
+        }
+        #expect(viewModel.count == -10)
+        #expect(!viewModel.minusButtonIsEnabled)
+        viewModel.decreaseCount()
+        #expect(viewModel.count == -10)
+        #expect(!viewModel.minusButtonIsEnabled)
+        viewModel.increaseCount()
+        #expect(viewModel.count == -9)
+        #expect(viewModel.minusButtonIsEnabled)
     }
 }
