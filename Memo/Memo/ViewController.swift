549c37f3a860646e0c9981f1099b289760d53a41
youseokhwan <youseokhwan15@gmail.com>
2025-02-25 15:36:03 +0900

    feat: TableView 추가

--- Memo/Memo/ViewController.swift
+++ Memo/Memo/ViewController.swift
@@ -1,9 +1,46 @@
 import UIKit
+import SnapKit
 
 final class ViewController: UIViewController {
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        return tableView
+    }()
+    private let dummyData = ["1", "2", "3"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureLayout()
+        configureConstraints()
+    }
+}
+
+private extension ViewController {
+    func configureLayout() {
         view.backgroundColor = .systemBackground
         title = "ViewController"
+
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
+    }
+
+    func configureConstraints() {
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dummyData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        cell.textLabel?.text = dummyData[indexPath.row]
+        return cell
     }
 }
