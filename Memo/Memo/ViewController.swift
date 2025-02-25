60fc9cf78cbce5fbd99dca9a8501d63eeed9873c
youseokhwan <youseokhwan15@gmail.com>
2025-02-25 15:51:04 +0900

    feat: 메모 추가 기능 구현

--- Memo/Memo/ViewController.swift
+++ Memo/Memo/ViewController.swift
@@ -6,7 +6,7 @@ final class ViewController: UIViewController {
         let tableView = UITableView()
         return tableView
     }()
-    private let dummyData = ["1", "2", "3"]
+    private var memos = ["1", "2", "3"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,18 +47,33 @@ private extension ViewController {
     }
 
     @objc func didTapAddMemoButton() {
-        print(#function)
+        let alert = UIAlertController(title: "새 메모", message: nil, preferredStyle: .alert)
+        alert.addTextField { textField in
+            textField.placeholder = "메모를 입력하세요"
+        }
+
+        let addAction = UIAlertAction(title: "추가", style: .default) { _ in
+            if let text = alert.textFields?.first?.text, !text.isEmpty {
+                self.memos.append(text)
+                self.tableView.reloadData()
+            }
+        }
+        let cancelAction = UIAlertAction(title: "취소", style: .destructive)
+        alert.addAction(addAction)
+        alert.addAction(cancelAction)
+
+        present(alert, animated: true)
     }
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dummyData.count
+        return memos.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
-        cell.textLabel?.text = dummyData[indexPath.row]
+        cell.textLabel?.text = memos[indexPath.row]
         return cell
     }
 }
