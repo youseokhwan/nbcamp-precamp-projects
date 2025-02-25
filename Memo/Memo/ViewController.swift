ddbc3204d6f43fe7cf462b765010c8a7db444070
youseokhwan <youseokhwan15@gmail.com>
2025-02-25 15:08:47 +0900

    feat: memo 프로젝트 생성

---
+++ Memo/Memo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Memo
+//
+//  Created by youseokhwan on 2/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
